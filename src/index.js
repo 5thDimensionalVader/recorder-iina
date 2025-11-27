8086ab52f4f6a075548f7f288c1418e49dfde104
Daniel Chinye <chinyedaniel@gmail.com>
2025-11-27 22:50:53 +0000

    feat: updated ui with inputs for current time & end time

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
 const {
   console,
   menu,
-  core
+  core,
+  event,
+  mpv
 } = iina;
 
 // load the react file
 const window = iina.standaloneWindow;
 
 // functions
-function postCurrentTimeMessage() {
+// function postCurrentTimeMessage(timeValue) {
+//   window.postMessage("currentTime", {
+//     time: timeValue,
+//   });
+// }
+
+function postEndTimeMessage () {
+  window.onMessage("getEndTime", () => {
+    let time = mpv.getNumber("time-pos");
+    window.postMessage("endTime", {
+      time: time,
+    });
+    core.pause();
+  })
+}
+
+// Add this function to get current time on demand
+function postCurrentTimeOnce() {
+  let time = mpv.getNumber("time-pos");
   window.postMessage("currentTime", {
-    time: core.status.position
+    time: time,
   });
 }
 
-
-window.setFrame(400, 200);
+window.setFrame(500, 300);
 window.loadFile("dist/ui/window/index.html");
-postCurrentTimeMessage();
-
+window.setProperty({title: "Start Recording"});
+postEndTimeMessage();
 
 // start recording menu item
 const startRecorderMenuItem = menu.item("Start Recording", () => {
   window.open();
+  postCurrentTimeOnce();
 }, {
   keyBinding: "Alt+Shift+r",
 })
 menu.addItem(startRecorderMenuItem);
+
+// events
+// event.on("mpv.time-pos.changed", () => {
+//   let time = mpv.getNumber("time-pos");
+//   postCurrentTimeMessage(time);
+// });
--- src/ui/window/app.jsx
+++ src/ui/window/app.jsx
@@ -1,24 +1,98 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { CssVarsProvider, CssBaseline } from "@mui/joy";
 
-const loadCurrentTime = () => {
-  return new Promise ((resolve, reject) => {
-    iina.onMessage("currentTime", ({time}) => resolve(time));
-  })
+// ui imports
+import Input from "@mui/joy/Input";
+import Stack from "@mui/joy/Stack";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Button from "@mui/joy/Button";
+
+// icons
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+
+async function getEndTime() {
+  return new Promise((resolve, reject) => {
+    iina.postMessage("getEndTime");
+    iina.onMessage("endTime", ({ time }) => resolve(time));
+  });
 }
+
 export default App = () => {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [currentMin, setCurrentMin] = useState(0);
+  const [currentSec, setCurrentSec] = useState(0);
+
+  const [endMin, setEndMin] = useState(0);
+  const [endSec, setEndSec] = useState(0);
+
+  function formatTime(positionInSec) {
+    const minutes = Math.floor(positionInSec / 60);
+    const seconds = Math.floor(positionInSec % 60);
+    return { minutes, seconds };
+  }
+
+  function handleSetEndTime() {
+    getEndTime().then((time) => {
+      const formattedTime = formatTime(time);
+      setEndMin(formattedTime.minutes);
+      setEndSec(formattedTime.seconds);
+    });
+  }
 
   useEffect(() => {
-    async function load () {
-      const time = await loadCurrentTime();
-      setCurrentTime(Math.floor(time / 60));
-    }
-    load();
+    const handleTimeUpdate = ({ time }) => {
+      const formattedTime = formatTime(time);
+      setCurrentMin(formattedTime.minutes);
+      setCurrentSec(formattedTime.seconds);
+    };
+    iina.onMessage("currentTime", handleTimeUpdate);
   }, []);
 
+  const startRecordingInputComponent = (
+    <FormControl>
+      <FormLabel>Current Time</FormLabel>
+      <Input
+        type="text"
+        variant="outlined"
+        startDecorator={<AddCircleIcon/>}
+        value={`${currentMin}:${currentSec}`}
+        readOnly
+      />
+    </FormControl>
+  );
+
+  const endRecordingInputComponent = (
+    <FormControl
+      sx={{
+        marginTop: 2,
+      }}
+    >
+      <FormLabel>End Time</FormLabel>
+      <Input
+        type="text"
+        variant="outlined"
+        startDecorator={<RemoveCircleIcon />}
+        value={`${endMin}:${endSec}`}
+        endDecorator={
+          <Button
+            variant="solid"
+            onClick={handleSetEndTime}
+          >
+            Set End Time
+          </Button>
+        }
+        readOnly
+      />
+    </FormControl>
+  );
+
   return (
-  <div>
-    <p>Current Time: { currentTime }</p>
-  </div>
-);
-}
+    <CssVarsProvider defaultMode="system">
+      <CssBaseline />
+      {startRecordingInputComponent}
+      {endRecordingInputComponent}
+    </CssVarsProvider>
+  );
+};
